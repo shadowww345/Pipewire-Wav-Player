aea49342ce3b0160789bbb544eba61430c14cc87
shadow <acaromer330@gmail.com>
2026-06-04 01:12:38 +0300

    Rename output binary to wav_player and update paths

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 
-gcc main.c player/eng_pipewire.c player/wav.c -o player -I./player $(pkg-config --cflags --libs libpipewire-0.3) -lm
+gcc main.c wav_player/eng_pipewire.c wav_player/wav.c -o wav_player -I./player $(pkg-config --cflags --libs libpipewire-0.3) -lm
 
